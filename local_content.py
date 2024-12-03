3a84a2844ab07df9b03140e508b3f8868b1b848d
Industry Research <info@research.research>
2024-12-04 08:50:30 +1100

    enriching list of questions

--- local_content.py
+++ local_content.py
@@ -12,6 +12,11 @@ def check_number(input):
             return True
         except ValueError:
             return False
+        
+@st.dialog(title="Save Results", width="small")
+def save(result):
+    st.write(f"The results have been saved!")
+    # insert value saving here
 
 production_values = {
     "All local production (100%)": 1.0, 
@@ -29,7 +34,7 @@ material_values = {
     "No local materials (0%)": 0.0,
 }
  
-st.title("⭐⭐ Content Calc ⭐⭐")
+st.title("Local Content Calculator")
 
 # creates a horizontal line
 st.write("---")
@@ -41,37 +46,82 @@ if "visibility" not in st.session_state:
 
 col1, col2 = st.columns(2)
 
+result = {
+    "company": "",
+    "email": "",
+    "good": "",
+    "material": "",
+    "production_level": "",
+    "material_level": "",
+    "production_weight": 0.0,
+    "material_weight": 0.0,
+    "overheads_margins_weight": 0.0,
+    "local_content_value": 0.0,
+}
+
 with col1:
+    # input company
+    result["company"] = st.text_input(
+        "Company Name",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder="ACME Corp.",
+    )
+
+    # input email
+    result["email"] = st.text_input(
+        "Email",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder="john@doe.com",
+    )
+
+    # input good
+    result["manufactured_good"] = st.text_input(
+        "Provide the name of your manufactured good",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder="Door, Window, Cable, Flooring, etc",
+    )
+    
+    # input good
+    result["material"] = st.text_input(
+        "What material is the item made out of? Provide any other relevant details",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder="Timber, concrete..",
+    )
+    
     # input 1
-    production_level = production_values[st.selectbox(
-        "First question",
+    result["production_level"] = production_values[st.selectbox(
+        "How much of the production is done within Australia and/or New Zealand? This includes research, design, manufacturing and/or construction.",
         (production_values.keys()),
     )]
 
     # input 2
-    material_level = material_values[st.selectbox(
-        "Second question",
+    result["material_level"] = material_values[st.selectbox(
+        "How much of the materials are sourced within Australia and/or New Zealand? This includes raw materials (e.g timber and metals), coatings (e.g solvents and paints) and electric components.",
         (material_values.keys()),
     )]
 
     # inputs 3
-    st.caption("Third question")
+    st.caption("What portion of the total cost do materials, production and overheads & margins account for? “Overheads & margins” include costs or profits spent/generated within ANZ, including logistics (e.g storage and transport), sale mark-up and any costs not covered under production and material.")
     
-    production_weight = st.text_input(
+    result["production_weight"] = st.text_input(
         "Production (percentage)",
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
         placeholder=str(33),
     )
 
-    material_weight = st.text_input(
+    result["material_weight"] = st.text_input(
         "Material (percentage)",
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
         placeholder=str(33),
     )
 
-    overheads_margins_weight = st.text_input(
+    result["overheads_margins_weight"] = st.text_input(
         "Overheads and Margins (percentage)",
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
@@ -80,14 +130,19 @@ with col1:
 
     # button magic
     if st.button("Compute", type="primary", use_container_width=True):
-        if check_number(production_weight) and check_number(material_weight) and check_number(overheads_margins_weight):
-            production_weight = float(production_weight)/100.0
-            material_weight = float(material_weight)/100.0
-            overheads_margins_weight = float(overheads_margins_weight)/100.0
-            if (production_weight)+(material_weight)+(overheads_margins_weight) != 1.0:
+        if check_number(result["production_weight"]) and check_number(result["material_weight"]) and check_number(result["overheads_margins_weight"]):
+            result["production_weight"] = float(result["production_weight"])/100.0
+            result["material_weight"] = float(result["material_weight"])/100.0
+            result["overheads_margins_weight"] = float(result["overheads_margins_weight"])/100.0
+            if (result["production_weight"])+(result["material_weight"])+(result["overheads_margins_weight"]) != 1.0:
                 col1.markdown('Please ensure that the percentages add up to a 100')
             else:
-                local_content_value = float((production_weight * production_level) + (material_weight * material_level) + (overheads_margins_weight * 1))
-                col2.markdown(f'# Final Percentage: {round(local_content_value * 100, 2)}%')
+                result["local_content_value"] = float((result["production_weight"] * result["production_level"]) + (result["material_weight"] * result["material_level"]) + (result["overheads_margins_weight"] * 1))
+                col2.markdown(f'# Final Percentage: {round(result["local_content_value"] * 100, 2)}%')
+                with col2:
+                    if st.button("Save", type="primary", use_container_width=True):
+                        # check here if the email address is valid
+                        save(result)
+
         else:
             col1.markdown('Please enter numbers for percentages')
